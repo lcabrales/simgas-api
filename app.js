6aaacbfc72162be72ecd8030fa1b5e294f3ab219
Lucas Cabrales <lucas_cabrales@hotmail.com>
2019-02-16 18:51:49 -0500

    Add Gas GET request

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ app.use(bodyParser.json())
 var sensorReadingRouter = require('./routes/sensor-reading');
 var sensorRouter = require('./routes/sensor');
 var airQualityRouter = require('./routes/air-quality');
+var gasRouter = require('./routes/gas');
 
 app.use('/SensorReading', sensorReadingRouter);
 app.use('/Sensor', sensorRouter);
 app.use('/AirQuality', airQualityRouter);
+app.use('/Gas', gasRouter);
 
 app.listen(3000, () => {
  console.log("Server running on port 3000");
