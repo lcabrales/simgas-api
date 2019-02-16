d361239f179f7a9c84418fb62de952c90a099856
Lucas Cabrales <lucas_cabrales@hotmail.com>
2019-02-16 18:55:50 -0500

    Add Location GET request

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ var sensorRouter = require('./routes/sensor');
 var airQualityRouter = require('./routes/air-quality');
 var gasRouter = require('./routes/gas');
 var boardRouter = require('./routes/board');
+var locationRouter = require('./routes/location');
 
 app.use('/SensorReading', sensorReadingRouter);
 app.use('/Sensor', sensorRouter);
 app.use('/AirQuality', airQualityRouter);
 app.use('/Gas', gasRouter);
 app.use('/Board', boardRouter);
+app.use('/Location', locationRouter);
 
 app.listen(3000, () => {
  console.log("Server running on port 3000");
