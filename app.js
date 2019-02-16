02622ec99fb4820cad70f975045f96e4dbfec0f9
Lucas Cabrales <lucas_cabrales@hotmail.com>
2019-02-16 18:25:01 -0500

    Add Sensor GET and POST definitions

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
 var sensorReadingRouter = require('./routes/sensor-reading');
+var sensorRouter = require('./routes/sensor');
+
 app.use('/SensorReading', sensorReadingRouter);
+app.use('/Sensor', sensorRouter);
 
 app.listen(3000, () => {
  console.log("Server running on port 3000");
---
+++ routes/sensor.js
@@ -0,0 +1,64 @@
+var express = require("express");
+var router = express.Router();
+var database = require("../database");
+var helper = require("../helper");
+
+var sql = database.sql;
+var config = database.config;
+
+router.get('/', function(req, res) {
+    console.log('receiving data ...');
+    console.log('query is ', req.query);
+
+    var query = req.query;
+
+    sql.connect(config).then(pool => {
+        return pool.request()
+        .input('SensorId', sql.UniqueIdentifier, query.SensorId)
+        .input('BoardId', sql.UniqueIdentifier, query.BoardId)
+        .execute('usp_Sensor_Get')
+    }).then(result => {
+        sql.close();
+
+        console.log(result)
+        res.json(helper.getResponseObject(result.recordset, 200, "OK"));
+    }).catch(err => {
+        sql.close();
+
+        console.log(err);
+        res.json(helper.getResponseObject(null, 500, "Un error ha ocurrido"))
+    })
+});
+
+router.post('/', function(req, res) {
+    console.log('receiving data ...');
+    console.log('body is ', req.body);
+
+    var body = req.body;
+
+    sql.connect(config).then(pool => {
+        return pool.request()
+        .input('BoardId', sql.UniqueIdentifier, body.BoardId)
+        .input('GasId', sql.UniqueIdentifier, body.GasId)
+        .input('Name', sql.VarChar, body.Name)
+        .input('ShortDescription', sql.VarChar, body.ShortDescription)
+        .input('FullDescription', sql.VarChar, body.FullDescription)
+        .input('LoadResistance', sql.Decimal, body.LoadResistance)
+        .input('ReceivedVolts', sql.Decimal, body.ReceivedVolts)
+        .input('CreatedBy', sql.UniqueIdentifier, body.CreatedBy)
+        .input('Status', sql.Bit, body.Status)
+        .execute('usp_Sensor_Create')
+    }).then(result => {
+        sql.close();
+
+        console.log(result)
+        res.json(helper.getResponseObject(result.recordset[0], 200, "OK"));
+    }).catch(err => {
+        sql.close();
+
+        console.log(err);
+        res.json(helper.getResponseObject(null, 500, "Un error ha ocurrido"));
+    })
+});
+
+module.exports = router;
