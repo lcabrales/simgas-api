f4a1d735ee9ffdec7a04a3bbea28910648da305e
Lucas Cabrales <lucas_cabrales@hotmail.com>
2019-05-04 18:31:02 -0500

    Implement the PUT User service

--- routes/user.js
+++ routes/user.js
@@ -61,4 +61,35 @@ router.post('/', function(req, res) {
     })
 });
 
+router.put('/', function(req, res) {
+    console.log('receiving data ...');
+    console.log('body is ', req.body);
+
+    var body = req.body;
+
+    var passwordHash = body.Password == null ? null : bcrypt.hashSync(body.Password)
+
+    sql.connect(config).then(pool => {
+        return pool.request()
+        .input('UserId', sql.UniqueIdentifier, body.UserId)
+        .input('RoleId', sql.UniqueIdentifier, body.RoleId)
+        .input('FirstName', sql.NVarChar, body.FirstName)
+        .input('LastName', sql.NVarChar, body.LastName)
+        .input('Email', sql.NVarChar, body.Email)
+        .input('Password', sql.NVarChar, passwordHash)
+        .input('LastModifiedBy', sql.NVarChar, body.LastModifiedBy)
+        .execute('usp_User_Update')
+    }).then(result => {
+        sql.close();
+
+        console.log(result)
+        res.json(helper.getResponseObject(result.recordset[0], 200, "OK"));
+    }).catch(err => {
+        sql.close();
+
+        console.log(err);
+        res.json(helper.getResponseObject(null, 500, "Un error ha ocurrido"));
+    })
+});
+
 module.exports = router;
