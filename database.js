43642e9b5636928aab7c0cb90b593e04eea37ba0
Lucas Cabrales <lucas_cabrales@hotmail.com>
2020-02-12 21:48:50 -0500

    v1.0.4 Update database credentials

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 
 var config = {
-  host     : 'ls-7f09f4cf79d05140044e361231125e68f520b9bd.cryas4khayxj.us-east-1.rds.amazonaws.com',
+  host     : 'ls-0a970c2647a89a5a7897529e031b54e28ce81b26.cryas4khayxj.us-east-1.rds.amazonaws.com',
   user     : 'dbmasteruser',
-  password : 'l+zM6;!Wt(Q]b7zyI[sPjKEFOCj*GU4o',
+  password : '>9Mh*|K7[3d<3mox^GTaN[ww[AoN-B~a',
   database : 'simgas'
 }
 
